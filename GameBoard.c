3a921d43d599b1323fca36787bc9fe5bcf3ea924
tomrichard3011 <54161487+tomrichard3011@users.noreply.github.com>
2020-11-17 12:48:07 -0800

    Added face functionality. Transforms 3d gameboard to 6 2d faces.

--- GameBoard.c
+++ GameBoard.c
@@ -1,10 +1,3 @@
-//
-// Created by ricky on 11/5/20.
-// Edited by nicolas on 11/8/20.
-// ^ I would remove this in future commits, git records for us
-//
-
-//#include "GameBoard.h"
 #include "stdio.h"
 #define gameBoardSize 3
 
@@ -16,6 +9,12 @@ void printGameBoard();
 
 // Numerical Gameboard
 int gameboard [gameBoardSize][gameBoardSize][gameBoardSize];
+int front[gameBoardSize][gameBoardSize];
+int back[gameBoardSize][gameBoardSize];
+int left[gameBoardSize][gameBoardSize];
+int right[gameBoardSize][gameBoardSize];
+int top[gameBoardSize][gameBoardSize];
+int bottom[gameBoardSize][gameBoardSize];
 
 // Print-friendly Gameboard
 char gameDisplay[592] = "\
@@ -38,21 +37,21 @@ I-----I-----I-----I";
 
 // These are the positions on the gameboard TODO: Make an array of pointers instead.
 int slots[gameBoardSize][gameBoardSize][gameBoardSize] = {
-    {
-        {351, 357, 363},
-        {473, 479, 485},
-        {555, 561, 567}
-    },
-    {
-        {250, 256, 262},
-        {372, 378, 384},
-        {494, 500, 506}
-    },
-    {
-        {148, 154, 160},
-        {270, 276, 282},
-        {392, 398, 404}
-    }
+        {
+                {351, 357, 363},
+                {473, 479, 485},
+                {555, 561, 567}
+        },
+        {
+                {250, 256, 262},
+                {372, 378, 384},
+                {494, 500, 506}
+        },
+        {
+                {148, 154, 160},
+                {270, 276, 282},
+                {392, 398, 404}
+        }
 };
 
 
@@ -64,6 +63,16 @@ void initGameBoard() {
         for (int j = 0; j < gameBoardSize; j++) {
             for (int k = 0; k < gameBoardSize; k++) {
                 gameboard[i][j][k] = 0;
+
+                front[i][j] = 0;
+                back[i][j] = 0;
+
+                top[i][j] = 0;
+                bottom[i][j] = 0;
+
+                left[i][j] = 0;
+                right[i][j] = 0;
+
             }
         }
     }
@@ -80,12 +89,14 @@ int isValidMove(int i, int j, int k) {
 int isValidIndex(int i, int j, int k) {
     if (i >= gameBoardSize || i < 0 ||
         j >= gameBoardSize || j < 0 ||
-        k >= gameBoardSize || k < 0
-            ) return 0;
-    return 1;
+        k >= gameBoardSize || k < 0 ||
+        (i == 1 && j == 1 && k == 1) // center of 3d cube
+            ) return 0; // return false;
+    return 1; // return true;
 }
 
 // gameboard[i][j][k]
+// but loop is j i k
 void printGameBoard() {
     for (int j = 0; j < gameBoardSize; j++) {
         for (int i = 0; i < gameBoardSize; i++) {
@@ -100,13 +111,13 @@ void printGameBoard() {
         printf("\n");
     }
 }
- 
+
 void displayBoard(){
-     printf("%s\n", gameDisplay);
-     //printf("%c", *(gameDisplay + 41)); // Location of C character
-     //printf("%c", *(gameDisplay + 148)); // Location of C character
-     //printf("%c", *(gameDisplay + 250)); // Location of C character
-     //printf("%c", *(gameDisplay + 351)); // Location of C character
+    printf("%s\n", gameDisplay);
+    //printf("%c", *(gameDisplay + 41)); // Location of C character
+    //printf("%c", *(gameDisplay + 148)); // Location of C character
+    //printf("%c", *(gameDisplay + 250)); // Location of C character
+    //printf("%c", *(gameDisplay + 351)); // Location of C character
 }
 
 /**
@@ -130,6 +141,22 @@ void findLocations(){
 void customEdit(){
     printf("\n%d", slots[0][0][0]);
     gameDisplay[slots[0][0][0]] = 'X';
+}
+
+// transfers game data to faces
+void updateFaces() {
+    for (int i = 0; i < gameBoardSize; i++) {
+        for (int j = 0; j < gameBoardSize; j++) {
+            front[i][j] = gameboard[0][i][j];
+            back[i][j] = gameboard[2][i][j];
+
+            top[i][j] = gameboard[i][0][j];
+            bottom[i][j] = gameboard[i][2][j];
+
+            left[i][j] = gameboard[i][j][0];
+            right[i][j] = gameboard[i][j][2];
+        }
+    }
 }
 
 // TODO 2d checkwin and 3d
